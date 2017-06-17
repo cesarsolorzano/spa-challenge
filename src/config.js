0a44218d35f1f94ab1d72927eb0ba0d8870a12a1
Cesar Solorzano <solorcesar@gmail.com>
2017-06-17 18:02:10 -0400

    Use https for the api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const apikey = '37755e5ca1ff4ecc52c3860e73afb03d';
 const hash = '27dc0e67e91e62c87aeb46bafbee294f';
-const apiPath = 'http://gateway.marvel.com/v1/';
+const apiPath = 'https://gateway.marvel.com/v1/';
 
 export { apikey, hash, apiPath };
